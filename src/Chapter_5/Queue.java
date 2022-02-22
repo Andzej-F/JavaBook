2900d7777290d63be27858102bf002036881a14c
Andzej-F <70884391+Andzej-F@users.noreply.github.com>
2022-02-23 00:26:00 +0200

    .

--- src/Chapter_5/Queue.java
+++ src/Chapter_5/Queue.java
@@ -6,7 +6,27 @@ public class Queue {
     int putloc, getloc;// the put and get indices
 
     public Queue(int size) {
-        this.q = new char[size];
-        putloc = getloc = 0;
+        this.q = new char[size];// allocate memory for queue
+        this.putloc = this.getloc = 0;
+    }
+
+    // out a character into the queue
+    public void put(char ch) {
+        if (this.putloc == q.length) {
+            System.out.println(" - Queue is full.");
+            return;
+        }
+
+        q[this.putloc++] = ch;
+    }
+
+    // get a character from the queue
+    public char get() {
+        if (this.getloc == this.putloc) {
+            System.out.println(" - Queue is empty.");
+            return (char) 0;
+        }
+
+        return q[this.getloc++];
     }
 }
---
+++ src/ExercisesNtuEduSg/SumAverageRunningInt.java
@@ -0,0 +1,52 @@
+package ExercisesNtuEduSg;
+
+public class SumAverageRunningInt {
+    public static void main(String[] args) {
+        int sum = 0;
+        int count = 0;
+        double average;
+        final int LOWERBOUND = 111;
+        final int UPPERBOUND = 8899;
+
+        for (int i = LOWERBOUND; i <= UPPERBOUND; i++) {
+            sum += i;
+            count++;
+        }
+//
+//        average = (double) sum / UPPERBOUND;
+
+//        int i = LOWERBOUND;
+//        while (i <= UPPERBOUND) {
+//            sum += i;
+//            i++;
+//        }
+
+//        int i = LOWERBOUND;
+//        do {
+//            sum += i;
+//            i++;
+//        } while (i <= UPPERBOUND);
+
+        average = (double) sum / UPPERBOUND;
+
+        System.out.println("The sum of " + LOWERBOUND + " to " + UPPERBOUND + " is " + sum);
+        System.out.println("The average is " + average);
+        System.out.println("There are " + count + " in the range");
+    }
+}
+/*Write a program called SumAverageRunningInt to produce the
+sum of 1, 2, 3, ..., to 100. Store 1 and 100 in variables lowerbound
+and upperbound, so that we can change their values easily. Also
+compute and display the average. The output shall look like:
+
+The sum of 1 to 100 is 5050
+The average is 50.5
+
+1. Modify the program to use a "while-do" loop instead of "for" loop.
+
+2. Modify the program to use a "do-while" loop.
+
+4. Modify the program to sum from 111 to 8899, and compute the average.
+Introduce an int variable called count to count the numbers in the
+specified range (to be used in computing the average).
+*/
